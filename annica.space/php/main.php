65ef64944ee6d4af598c7e505538e0503bc6a262
A A <annica@alienus.se>
2021-11-12 16:54:40 +0100

    Section About and Article for cv moved to file

--- annica.space/php/main.php
+++ annica.space/php/main.php
@@ -1,5 +1,5 @@
 <?php
-function get_html_section_intro(string $name, string $first_about, string $second_about = '')
+function get_html_section_intro(string $name, string $strAbout, int $splitStrAboutOn)
 {
     echo sprintf('
     <section id="intro" name="intro">
@@ -27,5 +27,31 @@ function get_html_section_intro(string $name, string $first_about, string $secon
         <!--/.container -->
     </div>
         <!--/ #intro -->
-</section>', $name, $first_about, $second_about);
+</section>', $name, substr($strAbout, 0, $splitStrAboutOn), substr($strAbout, 0));
+}
+
+function get_html_start_section_for_articles_cv(string $sectionId, string $sectionName = null, string $headValue)
+{
+    echo sprintf('<section id="%s" name="%s" class="divider">
+    <div class="container desc ">
+        <div class="row ">
+            <div class="col-lg-2 col-lg-offset-1">
+                <h2>%s</h2>
+            </div>', $sectionId, $sectionName, $headValue);
+}
+
+function get_html_article(string $className, string $firstheadValue, string $secondHeadValue = null, string $description, string $organisation, string $date)
+{
+    echo sprintf('
+    <article>
+        <div class="%s">
+            <h3 class=" ">%s</h3>
+            <h4>%s</h4>
+            <p>%s</p> 
+        </div>
+        <div class="col-lg-3">
+            <p class="edu-item">%s</p>
+            <p class="edu-year">%s</p> 
+        </div>
+    </article>', $className, $firstheadValue, $secondHeadValue, $description, $organisation, $date);
 }
