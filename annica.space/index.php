14f7380150b7409a25fcf3aa5de04f336a76bd36
Annica <annica@alienus.se>
2022-01-04 17:45:52 +0100

    smaller fixes

--- annica.space/index.php
+++ annica.space/index.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="utf-8" />
     <title>Annica Alienus</title>
-    <!-- <meta content="width=device-width, initial-scale=1.0" name="viewport" /> -->
-    <!-- <meta content="" name="keywords" /> -->
-    <!-- <meta content="" name="description" /> -->
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" /> -->
+    <meta content="" name="keywords" />
+    <meta content="" name="description" />
 
     <!-- Favicons -->
     <link href="img/apple-touch-icon.png" rel="apple-touch-icon" />
