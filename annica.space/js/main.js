841e8614adfba134dd19f17fdfbdc97769fda1c2
Annica <annica@alienus.se>
2022-03-21 16:51:57 +0100

    Added toggle class name on read more icon. Deleted previous code to m anage function

--- annica.space/js/main.js
+++ annica.space/js/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
-  document.getElementById("visible-item-id").innerText = "Visa mer...";
-
+  // document.getElementById("visible-item-id").innerText = "Visa mer...";
+  let lang;
   // Smooth scroll for the menu and links with .scrollto classes
   $(".smothscroll").on(
     "click",
@@ -26,15 +26,8 @@ jQuery(document).ready(function ($) {
     },
     // Toggle for the read more function
     $(".readmore").click(function () {
-      $header = $(this);
-
-      $(".hidden-item").slideToggle(600, function () {
-        $header.text(function () {
-          return $(this).text() == "Visa mindre"
-            ? "Visa mer..."
-            : "Visa mindre";
-        });
-      });
+      $("#readmore-icon").toggleClass("fas fa-chevron-down fas fa-chevron-up");
+      $(".hidden-item").slideToggle(600, function () {});
     })
   );
   jQuery("#lang-select").hover(
@@ -45,42 +38,4 @@ jQuery(document).ready(function ($) {
       jQuery(this).attr("size", 1);
     }
   );
-
-  // $("#lang-select")
-  //   .chosen()
-  //   .next(".chzn-container")
-  //   .hover(
-  //     function () {
-  //       $("#lang-select").trigger("liszt:open");
-  //     },
-  //     function () {
-  //       $(this).trigger("click");
-  //     }
-  //   );
-
-  $.post("../eng/index.php", { data: "value1" }, function (data) {
-    console.log(data);
-    $("#response").text(data);
-  });
-  // $("#lang-switch").change(function () {
-  //   // $('[lang="en"]').hide();
-  //   // $('[lang="sv"]').hide();
-  //   var lang = $(this).val();
-
-  //   console.log($(this).val());
-  //   switch (lang) {
-  //     case "en":
-  //       $("[lang]").hide();
-  //       $('[lang="en"]').show();
-
-  //       break;
-  //     case "sv":
-  //       // $("[lang]").hide();
-  //       $('[lang="sv"]').show();
-  //       break;
-  //     default:
-  //       // $("[lang]").hide();
-  //       $('[lang="sv"]').show();
-  //   }
-  // });
 });
